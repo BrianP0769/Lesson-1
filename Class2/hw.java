c1bda2bcdfee5e8623491dc9725902a06920347f
BrianP0769 <BrianP07869@gmail.com>
2019-02-24 15:11:00 -0500

    .

---
+++ Class2/hw.java
@@ -0,0 +1,17 @@
+public class lesson{
+  public static void main(String args[]){
+
+    int c = returnAverage(1, 1, 1);
+    System.out.println(c);
+
+  }
+
+
+
+
+
+  public static int returnAverage(int a, int b, int c){
+    int x = a+b+c;
+    return x/3;
+  }
+}
